f12a31f7ebc79393acc9df47ef6ef1afd7dd9ada
Kilian Agboton <kilian.agboton@gmail.com>
2021-03-21 23:58:52 -0400

    web-customer-tracker :: updated customer controller

--- src/io/kagboton/springdemo/controller/CustomerController.java
+++ src/io/kagboton/springdemo/controller/CustomerController.java
@@ -2,9 +2,11 @@ package io.kagboton.springdemo.controller;
 
 import io.kagboton.springdemo.dao.CustomerDAO;
 import io.kagboton.springdemo.entity.Customer;
+import io.kagboton.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,15 +15,15 @@ import java.util.List;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    // inject the customer dao
+    // inject the customer service
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model model){
 
-        // get customer from the dao
-        List<Customer> customers = customerDAO.getCustomers();
+        // get customers from the service
+        List<Customer> customers = customerService.getCustomers();
 
         // add the customers to the model
         model.addAttribute("customers", customers);
